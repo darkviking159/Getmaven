13c8364da89a7cfc9038a328ef51f6066233cee2
AppleDudeBrent <brent.schotte@realdolmen.com>
2018-09-24 13:08:37 +0200

    Java class

---
+++ src/main/java/BranchClass.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AppleDudeBrent
+ */
+public class BranchClass {
+    
+    private String naam;
+    private String voornaam;
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+    
+    
+    
+}
