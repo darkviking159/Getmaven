c53fe50f3487b3490fa99b378e0a3d2b8ea9bd59
AppleDudeBrent <brent.schotte@realdolmen.com>
2018-09-24 13:30:12 +0200

    Java class

---
+++ src/main/java/com/mycompany/getmaven/NeLekkerCowla.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.getmaven;
+
+/**
+ *
+ * @author AppleDudeBrent
+ */
+public class NeLekkerCowla {
+    
+    private String id;
+    
+    public void Test1 () {
+        System.out.println("Da is na nekeer ne lekkere COWLA ze!");
+    }
+}
